b8f4c23357761ef9d00044a3a0ca662a24d970f0
William Gustavo Santisteban <rootsantisteban@gmail.com>
2017-02-11 16:54:06 -0300

    Cambios

--- js/custom.js
+++ js/custom.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function() {
     //this code is for the gmap
 	 var map = new GMaps({
         el: '#map',
-        lat: -12.043333,
-        lng: -77.028333
+        lat: -33.0173404,
+        lng: -60.6795879
       });
 
 
